4c861506aa976f9011654263b22fe65bf218bc11
Ray Jeong <devops3199@gmail.com>
2022-02-27 18:42:00 +0900

    chore: update

--- wow-db/init/create.sql
+++ wow-db/init/create.sql
@@ -5,6 +5,7 @@ CREATE TABLE wow.user (
   `token` varchar(255) NOT NULL,
   `battleTag` varchar(50) NOT NULL,
   `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`,`token`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `token_UNIQUE` (`token`)
